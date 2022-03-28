bac831d9145ced34394007f4e62419f3f27281f0
yonatanski <phpcodeyg@gmail.com>
2022-03-28 13:21:24 +0200

    routes structure done

--- src/service/users/users.js
+++ src/service/users/users.js
@@ -1,6 +1,50 @@
-import express, {Router} from 'express'
+import express, { Router } from "express"
 
 const usersRouter = Router()
 
+// -----------------------------POST ROUTE------------------------
+
+usersRouter.post("/", async, (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error)
+  }
+})
+
+// -----------------------------GET ROUTE------------------------
+
+usersRouter.get("/", async, (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error)
+  }
+})
+
+// -----------------------------GET WITH ID ROUTE------------------------
+
+usersRouter.get("/", async, (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error)
+  }
+})
+
+// -----------------------------PUT ROUTE------------------------
+
+usersRouter.put("/", async, (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error)
+  }
+})
+
+// -----------------------------DELETE ROUTE------------------------
+
+usersRouter.delete("/", async, (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error)
+  }
+})
 
 export default usersRouter
