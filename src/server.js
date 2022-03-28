c89a1a98fdf1b6a6efe7739114e4c0a0beee538b
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-03-28 10:46:59 +0100

    cors setup completed

--- src/server.js
+++ src/server.js
@@ -2,32 +2,47 @@ import express from "express";
 import mongoose  from "mongoose";
 import listEndpoints from 'express-list-endpoints'
 import cors from 'cors'
-// import { badRequestHandler, genericErrorHandler, notFoundHandler, unauthorizedHandler } from "./service/handlers/errorHandler.js";
 import usersRouter from "./service/users/users.js";
 import chatsRouter from "./service/chats/chats.js";
+import { badRequestHandler, genericErrorHandler, notFoundHandler, unauthorizedHandler } from "./service/errors/errorHandlers.js";
 const {PORT = 3001} = process.env
 
 const server = express()
 
-server.use(cors())
+/********************** Middleware  ************************/
+const whiteList = [process.env.PROD_URL,process.env.DEV_URL]
+
+server.use(cors({origin:function(origin, next){
+    if(!origin || whiteListOrigins.indexOf(origin)!== -1){
+        next(null, true)
+    } else{
+        next(new Error("cors error"))
+    }
+}}))
+
 server.use(express.json())
 
 
+/********************** Routes  ************************/
 server.use("/users", usersRouter)
 server.use("/chats", chatsRouter)
 
+
+
+
+
+/**********************  ErrorHandles ************************/
+server.use(badRequestHandler)
+server.use(unauthorizedHandler)
+server.use(notFoundHandler)
+server.use(genericErrorHandler)
+
+/********************** Connections  ************************/
 
 mongoose.connect(process.env.MONGO_CONNECTION)
 mongoose.connection.on("connected", () => {
     console.log("successfully connected to  mongo!")
 })
-
-
-
-// server.use(badRequestHandler)
-// server.use(unauthorizedHandler)
-// server.use(notFoundHandler)
-// server.use(genericErrorHandler)
 
 server.listen(PORT, () => {
     console.table(listEndpoints(server))
---
+++ src/service/errors/errorHandlers.js
@@ -0,0 +1,24 @@
+
+export const badRequestHandler = (err, req, res, next) => {
+    if(err.status === 400 ){
+        res.status(400).send({ message: err.message })
+    }else{next(err)}}
+
+export const unauthorizedHandler = (err, req, res, next) => {
+    if(err.status === 401){
+        res.status(401).send({ message: err.message })
+    }else{next(err)
+    
+    }}
+
+export const notFoundHandler = (err, req, res, next) => {
+    if(err.status === 404){
+        res.status(404).send({ message: err.message })
+}else{
+    next(err)
+}}
+
+export const genericErrorHandler = (err, req, res, next) => {
+    console.log("Error handler - ", err)
+    res.status(500).send({ message: "Generic Server Error!" })
+}
